845b05a4c92c02ac0e232debe72018c4746faa27
Erick Lima <ericklcl@gmail.com>
2025-11-13 21:39:57 +0000

    modified r_shipment_stakeholders

--- models/raw/R_SHIPMENT_STAKEHOLDERS.sql
+++ models/raw/R_SHIPMENT_STAKEHOLDERS.sql
@@ -6,6 +6,7 @@
   )
 }}
 
+WITH STAGED AS (
 SELECT    
     t.$1:fourKitesShipmentID::VARCHAR AS SHIPMENT_ID,
     f.KEY   AS PARTY_ROLE,
@@ -23,3 +24,25 @@ SELECT
     md5(to_json(t.$1)) as _META_ROW_HASH
 from {{ source('RAW','R_FOURKITES_JSON_PAYLOAD') }} AS t,
      LATERAL FLATTEN(INPUT => t.$1:stakeholders) f
+)
+SELECT
+    SRC.SHIPMENT_ID,
+    SRC.PARTY_ROLE,
+    SRC.PARTY_NAME,
+    SRC._SYSTEM_ID,
+    SRC._STAGE_ID,
+    SRC._META_FILENAME,
+    SRC._META_ROW_NUMBER,
+    SRC._META_FILE_LAST_MODIFIED,
+    SRC._META_INGESTION_TIMESTAMP,
+    SRC._META_ROW_HASH
+FROM STAGED AS SRC
+{% if is_incremental() %}
+where NOT EXISTS (
+    select 1
+    from {{ this }} as BASE
+    where  SRC._META_FILENAME = BASE._META_FILENAME
+      and SRC._META_FILE_LAST_MODIFIED = BASE._META_FILE_LAST_MODIFIED
+      and SRC.SHIPMENT_ID = BASE.SHIPMENT_ID
+)
+{% endif %}
