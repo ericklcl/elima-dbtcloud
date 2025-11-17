93f60b40bd3d2813d984db637be3b8f212b243d8
Erick Lima <ericklcl@gmail.com>
2025-11-17 16:19:16 +0000

    fix snowflake query

--- models/raw/R_STOP_CUSTOMERS.sql
+++ models/raw/R_STOP_CUSTOMERS.sql
@@ -20,7 +20,7 @@ WITH SRC AS (
         MD5(TO_JSON($1)) AS _META_ROW_HASH
 
         
-    FROM FROM {{ source('RAW','R_FOURKITES_JSON_PAYLOAD') }}
+    FROM {{ source('RAW','R_FOURKITES_JSON_PAYLOAD') }}
 )
 SELECT
     f.VALUE:fourKitesStopID::VARCHAR AS STOP_ID,
