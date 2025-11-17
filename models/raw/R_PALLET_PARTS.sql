fdd15151f9c2f89750146cca625dce0a21d558c5
Erick Lima <ericklcl@gmail.com>
2025-11-17 17:56:43 +0000

    create new model

--- models/raw/R_PALLET_PARTS.sql
+++ models/raw/R_PALLET_PARTS.sql
@@ -6,62 +6,125 @@
     post_hook="{{ apply_table_and_column_comments(this) }}"
 ) }}
 
-WITH PALLETS AS (
-    SELECT *
-    FROM {{ ref('R_STOP_PALLETS') }}   -- your pallet model
+-- 1) Read source table
+WITH SRC AS (
+    SELECT
+        payload,
+        _system_id,
+        _stage_id,
+        _meta_filename,
+        _meta_row_number,
+        _meta_file_last_modified,
+        _meta_ingestion_timestamp,
+        MD5(TO_JSON(payload)) AS _meta_row_hash
+    FROM {{ source('RAW','R_FOURKITES_JSON_PAYLOAD') }}
 ),
 
+-- 2) Flatten stops
+STOPS AS (
+    SELECT
+        f_stop.value AS STOP_OBJ,
+        f_stop.value:fourKitesStopID::varchar AS STOP_ID,
+
+        SRC._system_id,
+        SRC._stage_id,
+        SRC._meta_filename,
+        SRC._meta_row_number,
+        SRC._meta_file_last_modified,
+        SRC._meta_ingestion_timestamp,
+        SRC._meta_row_hash
+
+    FROM SRC,
+         LATERAL FLATTEN(input => payload:stops) f_stop
+),
+
+-- 3) Flatten pallets
+PALLETS AS (
+    SELECT
+        s.STOP_ID,
+        s.STOP_OBJ,
+        pallet.value AS PALLET_OBJ,
+
+        ROW_NUMBER() OVER (
+            PARTITION BY s.STOP_ID, s._meta_filename
+            ORDER BY 1
+        ) AS PALLET_SEQ_RAW,
+
+        s._system_id,
+        s._stage_id,
+        s._meta_filename,
+        s._meta_row_number,
+        s._meta_file_last_modified,
+        s._meta_ingestion_timestamp,
+        s._meta_row_hash
+
+    FROM STOPS s,
+         LATERAL FLATTEN(input => TO_ARRAY(s.STOP_OBJ:pallets)) pallet
+    WHERE ARRAY_SIZE(OBJECT_KEYS(pallet.value)) > 0
+),
+
+-- 4) Flatten parts
 PARTS AS (
     SELECT
         p.STOP_ID,
-        p.PALLET_SEQ AS PALLET_SEQ,
+        p.PALLET_OBJ,
+        p.PALLET_SEQ_RAW,
         part.value AS PART_OBJ,
-        part.seq AS RAW_PART_SEQ,        
-        p._SYSTEM_ID,
-        p._META_FILENAME,
-        p._META_INGESTION_TIMESTAMP,
-        p._META_FILE_LAST_MODIFIED
+
+        ROW_NUMBER() OVER (
+            PARTITION BY p.STOP_ID, p._meta_filename, p.PALLET_SEQ_RAW
+            ORDER BY 1
+        ) AS PART_SEQ_RAW,
+
+        p._system_id,
+        p._stage_id,
+        p._meta_filename,
+        p._meta_row_number,
+        p._meta_file_last_modified,
+        p._meta_ingestion_timestamp,
+        p._meta_row_hash
+
     FROM PALLETS p,
-         LATERAL FLATTEN(input => p.PALLET_OBJ:parts) part
-    WHERE ARRAY_SIZE(OBJECT_KEYS(part.value)) > 0   
+         LATERAL FLATTEN(input => TO_ARRAY(p.PALLET_OBJ:parts)) part
+    WHERE ARRAY_SIZE(OBJECT_KEYS(part.value)) > 0
 )
 
+-- 5) Final output
 SELECT
     STOP_ID,
-    PALLET_SEQ,
+
     ROW_NUMBER() OVER (
-        PARTITION BY STOP_ID, PALLET_SEQ, _META_FILENAME
-        ORDER BY RAW_PART_SEQ
+        PARTITION BY STOP_ID, _META_FILENAME
+        ORDER BY PALLET_SEQ_RAW
+    ) AS PALLET_SEQ,
+
+    ROW_NUMBER() OVER (
+        PARTITION BY STOP_ID, _META_FILENAME, PALLET_SEQ_RAW
+        ORDER BY PART_SEQ_RAW
     ) AS PART_SEQ,
 
-    PART_OBJ:description::VARCHAR              AS DESCRIPTION,
-    PART_OBJ:shipperPartNumber::VARCHAR        AS SHIPPER_PART_NUMBER,
+    PART_OBJ:description::varchar       AS DESCRIPTION,
+    PART_OBJ:shipperPartNumber::varchar AS SHIPPER_PART_NUMBER,
 
-    -- raw strings
-    CASE WHEN PART_OBJ:quantity IS NULL THEN NULL 
-         ELSE PART_OBJ:quantity::VARCHAR END   AS QUANTITY_RAW,
+    CASE WHEN PART_OBJ:quantity IS NULL THEN NULL
+         ELSE PART_OBJ:quantity::varchar
+    END AS QUANTITY_RAW,
 
-    TRY_TO_NUMBER(PART_OBJ:quantity::VARCHAR)  AS QUANTITY_NUM,
+    TRY_TO_NUMBER(PART_OBJ:quantity::varchar) AS QUANTITY_NUM,
 
-    CASE WHEN PART_OBJ:weight IS NULL THEN NULL 
-         ELSE PART_OBJ:weight::VARCHAR END     AS WEIGHT_RAW,
+    CASE WHEN PART_OBJ:weight IS NULL THEN NULL
+         ELSE PART_OBJ:weight::varchar
+    END AS WEIGHT_RAW,
 
-    TRY_TO_NUMBER(PART_OBJ:weight::VARCHAR)    AS WEIGHT_NUM,
+    TRY_TO_NUMBER(PART_OBJ:weight::varchar) AS WEIGHT_NUM,
 
-    _SYSTEM_ID,
-    _META_FILENAME,
-    _META_INGESTION_TIMESTAMP,
-    _META_FILE_LAST_MODIFIED
+    -- ALL METADATA FIELDS:
+    _system_id,
+    _stage_id,
+    _meta_filename,
+    _meta_row_number,
+    _meta_file_last_modified,
+    _meta_ingestion_timestamp,
+    _meta_row_hash
 
 FROM PARTS
-
-{% if is_incremental() %}
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM {{ this }} BASE
-    WHERE BASE.STOP_ID = PARTS.STOP_ID
-      AND BASE.PALLET_SEQ = PARTS.P_IDX
-      AND BASE._META_FILENAME = PARTS._META_FILENAME
-      AND BASE._META_FILE_LAST_MODIFIED = PARTS._META_FILE_LAST_MODIFIED
-)
-{% endif %}
